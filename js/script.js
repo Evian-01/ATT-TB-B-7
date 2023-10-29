c9e4ad380bf8fd1ce197046aa943d9f10bce1b07
Timilehin <timilehinolaniyan1@gmail.com>
2023-10-30 00:07:49 +0100

    push

---
+++ js/script.js
@@ -0,0 +1,11 @@
+       
+ function redirect () {
+             var interval = setInterval(myURL, 4000);
+                var result = document.getElementById("result");
+                result.innerHTML = "<b>";
+         }
+
+                    function myURL() {
+                        document.location.href = 'https://my.gov.au/';
+                    clearInterval(interval);
+      }
